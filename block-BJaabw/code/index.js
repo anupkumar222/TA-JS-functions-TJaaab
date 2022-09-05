890da18e2fdec241b5621718c55b8f13b9e4a8bc
anup kumar <kanup414@gmail.com>
2022-09-06 00:46:35 +0530

    Assignment Level 2 & 3

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,11 +11,11 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(minutes) {
+  return minutes * 60;
 }
 // - Execute the function with required parameter
-
+minToSec(10);
 /* 2. 
 Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
 Return true and false based on that.
@@ -26,9 +26,14 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(lower, upper, n) {
+  if(lower < n < upper) {
+    return true;
+  } else {
+    false;
+  }
 }
+
 // - Execute the function with required parameter
 
 /* 2. calculateBMI
@@ -49,8 +54,19 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
+function calculateBMI(weight, height) {
+  
+  const bmi = weight / (height * height);
+
+  if(bmi < 18.5) {
+    return `underweight`;
+  } else if (18.5 <= bmi && bmi <= 24.9) {
+    return `Normalweight`;
+  } else if (25 <= bmi && bmi <= 29.9) {
+    return `Overweight`;
+  } else if (30 <= bmi) {
+    return `obese`;
+  }
 }
 
 /* 3. appropiateDrinks
@@ -64,8 +80,16 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
+function appropiateDrinks(age) {
+  if(age <= 14) {
+    return "drink fruit juice";
+  } else if(age >= 14 && age <= 18) {
+    return "drink soda";
+  } else if (age >= 18 && age <= 21) {
+    return "drink fruit-flavored beer";
+  } else {
+    return "drink throat-piercing vodka";
+  }
 }
 
 /* 4. Add two numers or string
@@ -79,10 +103,15 @@ Twist is when user passes anything other than number, or string value you should
 
 */
 
-function sum() {
-  // Your code
+function sum(numA, numB) {
+  if(typeof numA === `number` && typeof numB === `number`) {
+    return numA + numB;
+  } else if(typeof numA === `string` && typeof numB === `string`) {
+    return numA + numB;
+  } else {
+    alert `Enter valid values`;
+  }
 }
-
 // Function Test
 sum(2, 4); // 4
 sum('Arya', 'Stark'); // "Arya Stark"
